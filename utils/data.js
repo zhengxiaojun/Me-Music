21e9b05df6e90b23eb154e3c83c8cb565ecaf728
zhengxiaojun <zhengxiaojun@example.com>
2021-12-04 07:58:37 +0800

    first commit

---
+++ utils/data.js
@@ -0,0 +1,16 @@
+export default [{
+	src: 'http://139.196.136.137/resources/自然界音乐.mp3',
+	poster: 'http://139.196.136.137/resources/bg.png',
+	name: '自然界音乐',
+	author: '杰克'
+}, {
+	src: 'http://139.196.136.137/resources/睡眠音乐.mp3',
+	poster: 'http://139.196.136.137/resources/bg.png',
+	name: '睡眠音乐',
+	author: '杰克'
+}, {
+	src: 'http://139.196.136.137/resources/古典音乐.mp3',
+	poster: 'http://139.196.136.137/resources/bg.png',
+	name: '古典音乐',
+	author: '杰克'
+}]

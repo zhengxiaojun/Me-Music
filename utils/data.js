f9b0acec5eb63b2a05e73923c9692fa6a6e10a80
zhengxiaojun <zhengxiaojun@example.com>
2021-12-04 09:17:37 +0800

    fixed mp3 can't play with zh_cn

--- utils/data.js
+++ utils/data.js
@@ -1,15 +1,15 @@
 export default [{
-	src: 'http://139.196.136.137/resources/自然界音乐.mp3',
+	src: 'http://139.196.136.137/resources/nature.mp3',
 	poster: 'http://139.196.136.137/resources/bg.png',
 	name: '自然界音乐',
 	author: '杰克'
 }, {
-	src: 'http://139.196.136.137/resources/睡眠音乐.mp3',
+	src: 'http://139.196.136.137/resources/sleep.mp3',
 	poster: 'http://139.196.136.137/resources/bg.png',
 	name: '睡眠音乐',
 	author: '杰克'
 }, {
-	src: 'http://139.196.136.137/resources/古典音乐.mp3',
+	src: 'http://139.196.136.137/resources/classical.mp3',
 	poster: 'http://139.196.136.137/resources/bg.png',
 	name: '古典音乐',
 	author: '杰克'

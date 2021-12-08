4b0a310628a76b754ec6b50d89d3890347acf0e0
zhengxiaojun <zhengxiaojun@example.com>
2021-12-08 22:08:31 +0800

    fixed some

--- utils/data.js
+++ utils/data.js
@@ -1,16 +1,33 @@
 export default [{
-	src: 'http://139.196.136.137/resources/nature.mp3',
-	poster: 'http://139.196.136.137/resources/bg.png',
-	name: '自然界音乐',
-	author: '杰克'
-}, {
-	src: 'http://139.196.136.137/resources/sleep.mp3',
-	poster: 'http://139.196.136.137/resources/bg.png',
-	name: '睡眠音乐',
-	author: '杰克'
-}, {
-	src: 'http://139.196.136.137/resources/classical.mp3',
-	poster: 'http://139.196.136.137/resources/bg.png',
-	name: '古典音乐',
-	author: '杰克'
-}]
+		src: 'http://139.196.136.137/resources/nature.mp3',
+		poster: 'http://139.196.136.137/resources/bg.png',
+		name: '自然界音乐',
+		author: '杰克'
+	}, {
+		src: 'http://139.196.136.137/resources/sleep.mp3',
+		poster: 'http://139.196.136.137/resources/bg.png',
+		name: '睡眠音乐',
+		author: '杰克'
+	}, {
+		src: 'http://139.196.136.137/resources/sky.mp3',
+		poster: 'http://139.196.136.137/resources/sky.jpg',
+		name: '天空之城',
+		author: '久石让'
+	}, {
+		src: 'http://139.196.136.137/resources/qyqx.mp3',
+		poster: 'http://139.196.136.137/resources/qyqx.jpg',
+		name: '千与千寻',
+		author: '久石让'
+	}, {
+		src: 'http://139.196.136.137/resources/jcldxt.mp3',
+		poster: 'http://139.196.136.137/resources/jcldxt.jpg',
+		name: '菊次郎的夏天',
+		author: '久石让'
+	}
+	//  {
+	// 	src: 'http://139.196.136.137/resources/classical.mp3',
+	// 	poster: 'http://139.196.136.137/resources/bg.png',
+	// 	name: '古典音乐',
+	// 	author: '杰克'
+	// }
+]
